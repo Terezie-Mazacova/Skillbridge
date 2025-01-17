c1646ac2c07dc0ba0f47db7981f533cd50bfdcb6
Terezie Mazáčová <terezkamazacova@post.cz>
2025-01-17 22:43:39 +0100

    Hotová sekce FAQ v main page

--- script.js
+++ script.js
@@ -45,3 +45,12 @@ jsTabs.forEach((jsTab) =>{
         });
     });
 })
+
+// == FAQ ==
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle ('active')
+    })
+})
